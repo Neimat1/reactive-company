a0f3002345d4f1a589e4409f17995aeeee68ba60
Neimat1 <neimat.soliman.ismail@gmail.com>
2024-10-17 19:44:42 +0300

    rest-client: add media type on each method

--- rest-client/src/main/java/com/quarkus/service/two/rest/CompanyClient.java
+++ rest-client/src/main/java/com/quarkus/service/two/rest/CompanyClient.java
@@ -6,32 +6,37 @@ import io.smallrye.mutiny.Uni;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 import java.util.List;
 
 @RegisterRestClient(configKey = "company.client")
-@Path("/api/employees")
-@Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
+@Path("/employees")
 public interface CompanyClient {
 
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
     Uni<List<Employee>> getAllEmployees();
 
     @GET
     @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
     Uni<Employee> getEmployeeById(@PathParam("id") Long id);
 
     @POST
-    Uni<Response> addEmployee(Employee employee);
+    @Consumes(MediaType.APPLICATION_JSON)
+    Uni<Response> addEmployee(@RequestBody Employee employee);
 
     @PUT
     @Path("/{id}")
-    Uni<Employee> updateEmployeePosition(@PathParam("id") Long id, String updatedPosition);
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    Uni<Employee> updateEmployeePosition(@PathParam("id") Long id, @RequestBody String updatedPosition);
 
     @DELETE
     @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
     Uni<Boolean> removeEmployee(@PathParam("id") Long employeeId);
 
 
